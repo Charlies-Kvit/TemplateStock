70d65ca957560e31c363777d3441d20521ece656
froon-rr <84666873+froon-rr@users.noreply.github.com>
2023-04-17 16:53:07 +0300

    add posts.py

---
+++ data/posts.py
@@ -0,0 +1,22 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+from flask_login import UserMixin
+
+
+class Posts(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'posts'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship('User')
+    news = orm.relationship("Posts", back_populates='user')
