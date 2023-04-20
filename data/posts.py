0e5fc8fe50994c56fb8dfe3ae2c47be12ac16417
froon-rr <84666873+froon-rr@users.noreply.github.com>
2023-04-20 17:24:26 +0300

    Update posts.py

--- data/posts.py
+++ data/posts.py
@@ -1,12 +1,11 @@
 import datetime
 import sqlalchemy
-from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
-from flask_login import UserMixin
+from .db_session import SqlAlchemyBase
 
 
-class Posts(SqlAlchemyBase, UserMixin):
-    __tablename__ = 'posts'
+class Post(SqlAlchemyBase):
+    __tablename__ = 'post'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
@@ -19,3 +18,4 @@ class Posts(SqlAlchemyBase, UserMixin):
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
+
